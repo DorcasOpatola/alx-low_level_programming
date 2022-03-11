3bd85630015d751ae4c98f408efc2bf2ef81d0b0
Oluwaseun 007 <preciousopal.od@gmail.come>
2022-03-10 19:23:57 -0800

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+include <stdiod.h>
+
+/**
+ * *Main - Entry point
+ * *Description: prints "Programming is like building a multilingual puzzle
+ * *Return: Always 0 (success)
+ * **/
+
+Int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
+
