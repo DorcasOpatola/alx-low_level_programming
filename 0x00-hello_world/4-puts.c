acb21c9249cb5c2a54b69ece363d211784a8c527
Oluwaseun 007 <preciousopal.od@gmail.come>
2022-03-10 20:12:01 -0800

    Betic

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
-include <stdiod.h>
+#include <stdio.h>
 
 /**
- * *Main - Entry point
- * *Description: prints "Programming is like building a multilingual puzzle
- * *Return: Always 0 (success)
- * **/
-
-Int main(void)
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
 
